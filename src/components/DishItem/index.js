28f42a95794c0fc4f3629a64b04318572866105f
rishik <rishibalanagu0108@gmail.com>
2024-12-13 10:24:44 +0000

    Solution

--- src/components/DishItem/index.js
+++ src/components/DishItem/index.js
@@ -30,12 +30,12 @@ const DishItem = props => {
   }
 
   const renderControllerButton = () => (
-    <div className="controller-container">
-      <button className="button" type="button" onClick={onDecreaseQuantity}>
+    <div className='controller-container'>
+      <button className='button' type='button' onClick={onDecreaseQuantity}>
         -
       </button>
-      <p className="quantity">{quantity}</p>
-      <button className="button" type="button" onClick={onIncreaseQuantity}>
+      <p className='quantity'>{quantity}</p>
+      <button className='button' type='button' onClick={onIncreaseQuantity}>
         +
       </button>
     </div>
@@ -46,38 +46,38 @@ const DishItem = props => {
   }
 
   return (
-    <li className="dish-item-container">
+    <li className='dish-item-container'>
       <div
         className={`veg-border ${dishType === 1 ? 'non-veg-border' : ''} me-3`}
       >
         <div className={`veg-round ${dishType === 1 ? 'non-veg-round' : ''}`} />
       </div>
-      <div className="dish-details-container">
-        <h1 className="dish-name">{dishName}</h1>
-        <p className="dish-currency-price">
+      <div className='dish-details-container'>
+        <h1 className='dish-name'>{dishName}</h1>
+        <p className='dish-currency-price'>
           {dishCurrency} {dishPrice}
         </p>
-        <p className="dish-description">{dishDescription}</p>
+        <p className='dish-description'>{dishDescription}</p>
         {dishAvailability && renderControllerButton()}
         {!dishAvailability && (
-          <p className="not-availability-text">Not available</p>
+          <p className='not-availability-text'>Not available</p>
         )}
 
         {addonCat.length > 0 && (
-          <p className="addon-availability-text">Customizations available</p>
+          <p className='addon-availability-text'>Customizations available</p>
         )}
         {quantity > 0 && (
           <button
-            type="button"
-            className="button add-cart"
+            type='button'
+            className='button add-cart'
             onClick={onAddCartItem}
           >
             ADD TO CART
           </button>
         )}
       </div>
-      <p className="dish-calories">{dishCalories} calories</p>
-      <img src={dishImage} alt={dishName} className="dish-image" />
+      <p className='dish-calories'>{dishCalories} calories</p>
+      <img src={dishImage} alt={dishName} className='dish-image' />
     </li>
   )
 }
